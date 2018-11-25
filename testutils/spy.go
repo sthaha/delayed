cf1675476b9b1749a070c6ef80800d70f17fb1eb
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2018-11-26 01:41:35 +1000

    testutils/spy: add HookSync to wait for hook to finish

--- testutils/spy.go
+++ testutils/spy.go
@@ -8,21 +8,45 @@ import (
 
 type Args map[string]interface{}
 
+type Info struct {
+	Called int
+	Arg    Args
+}
+
 // Spy is used to inspect how many times hooks are called
 type Spy struct {
 	m      sync.RWMutex
 	calls  int
 	fnArgs Args
+	Done   chan Info
 }
 
 func (s *Spy) Hook(v Args) func() {
-	return func() {
-		s.m.Lock()
-		defer s.m.Unlock()
-		s.calls++
-		s.fnArgs = v
-		Logger("spy")("called [%d] times with args: %s", s.calls, spew.Sdump(v))
+	return func() { s.updateCalled(v) }
+}
+
+func (s *Spy) HookSync() (func(v Args) func(), chan Info) {
+	ch := make(chan Info)
+
+	hook := func(v Args) func() {
+		return func() {
+			s.updateCalled(v)
+			select {
+			case ch <- Info{s.calls, v}:
+			default:
+			}
+
+		}
 	}
+	return hook, ch
+}
+
+func (s *Spy) updateCalled(v Args) {
+	s.m.Lock()
+	defer s.m.Unlock()
+	s.calls++
+	s.fnArgs = v
+	Logger("spy")("called [%d] times with args: %s", s.calls, spew.Sdump(v))
 }
 
 func (s *Spy) Called() int {
